55c63dc993ce474f10381ec6388cc70819de2bea
Ullas Js <ullasjskadakkal75@gmail.com>
2023-11-21 23:05:00 +0530

    added header/sidebar

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 import "./styles/header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CgMenuGridO } from "react-icons/cg";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { useState } from "react";
 
 export function Header() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="header">
       <nav>
@@ -32,7 +39,46 @@ export function Header() {
               </Link>
             </li>
           </ul>
-          <div className="sidebar-open">hi</div>
+          <div className="sidebar-open">
+            <CgMenuGridO
+              className="menu-btn"
+              variant="primary"
+              onClick={handleShow}
+            />
+            <Offcanvas className="canvas-off" show={show} onHide={handleClose}>
+              <Offcanvas.Header >
+                <Offcanvas.Title className="side-logo">
+                  Ulla<span className="acsent-color">s</span>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <div className="sidebar">
+                  <ul className="sidemenu">
+                    <li>
+                      <Link to="/" className="side-links">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/projects" className="side-links">
+                        Projects
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/aboutme" className="side-links">
+                        About Me
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/contact" className="side-links">
+                        Contacts
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </Offcanvas.Body>
+            </Offcanvas>
+          </div>
         </div>
       </nav>
     </div>
