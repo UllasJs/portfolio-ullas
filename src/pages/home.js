2b00d7ae0244ad6bb0357f98502a6abde5a2aff4
Ullas Js <ullasjskadakkal75@gmail.com>
2023-11-23 21:48:25 +0530

    added home page except responsive ness

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import "./styles/home.css";
+import Portrait from "../assets/IMG-20230129-WA0004.jpg";
 import { BsArrowRightCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
@@ -7,21 +8,27 @@ function Home() {
     <div className="homePage">
       <section className="home-section">
         <div className="home-row">
-          <div className="home-col">
+          <div className="home-col text-col">
             <h1>
-              It's Me <span>Ullas</span>
+              It's Me <br />
+              <span className="text-span-primary main-text">Ullas </span> JS
             </h1>
             <h4>
-              Im Into <span>Front End Developer</span>
+              I'm Into
+              <span className="text-span-primary"> Front End Developer</span>
             </h4>
-            <Link to="/about">
+            <Link className="about-link" to="/about">
               About Me
-              <span>
+              <span className="about-icon">
                 <BsArrowRightCircleFill />
               </span>
             </Link>
           </div>
-          <div className="home-col"></div>
+          <div className="home-col">
+            <div className="profile-pic">
+              <img src={Portrait} alt="" />
+            </div>
+          </div>
         </div>
       </section>
     </div>
